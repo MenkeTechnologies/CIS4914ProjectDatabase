300ad1f1ceb37f3f00e74342fee216fc21185292
MenkeTechnologies <linux.dev25@gmail.com>
2022-01-24 00:29:06 -0500

    gitignore

--- start.sh
+++ start.sh
@@ -1,14 +1,17 @@
-trap 'kill $pid;exit 1' INT
+trap 'kill $pid;exit' INT
 
 if [[ -n $1 ]]; then
   cd backend
+  npm i
   npm run start
 else
   cd backend
+  npm i
   npm run start &
   pid=$!
   cd ..
   cd frontend
+  npm i
   npm run start
 
 fi
